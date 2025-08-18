ccd8ad379ab0c997b3903d3ccc578d24f366799d
Momen Rabah <momenrx@gmail.com>
2025-08-18 15:22:57 +0300

    fix: prisma

--- backend/api/utils/prisma.ts
+++ backend/api/utils/prisma.ts
@@ -1,5 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ['query', 'error', 'warn'],
+  });
 
 export default prisma;
